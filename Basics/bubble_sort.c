fc38bcc9ce899aaff71fcfdcd03740ae7ea506b6
InfinityBotvcoder <79300402+InfinityBotvcoder@users.noreply.github.com>
2026-02-11 07:13:31 -0800

    Create bubble_sort.c

---
+++ Basics/bubble_sort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int comparision = 0;
+int swap = 0;
+
+void bubble_sort(int arr[],int n){
+    int i,j,temp;
+    for(i=0;i<n-1;i++){
+        int flag=0;
+        for(j=i+1;j<n;j++){
+            comparision++;
+            if(arr[i]>arr[j])
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                swap++;
+                flag++;
+            
+        }
+        if(flag==0) break;
+    }
+
+
+}
+
+int main(){
+    int i,n,num;
+    printf("Enter number of inputs:");
+    scanf("%d",&n);
+    int arr[n];
+
+    for(i=0;i<n;i++){
+        printf("Enter number for position %d ",i);
+        scanf("%d",&num);
+        arr[i] = num;
+    }
+
+    bubble_sort(arr,n);
+
+    printf("Sorted array: \n");
+    for(i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("Swap_Count: %d Comparision_Count: %d",swap,comparision);
+   
+   
+    return 0;
+}
